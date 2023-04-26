2bf7d0045066f9d90a21641e8aedfde97e6c9475
mistertandon <enggparveshtandon@gmail.com>
2023-04-26 23:08:45 +0530

    feat: PubSub design pattern implementation

---
+++ footer.js
@@ -0,0 +1,8 @@
+import { pubsub } from './pubsub.js';
+
+export const footer = {
+  render: function(container) {
+    //if we wanted html stuff...do it here
+  },
+  itemsUpdated: function(item) {}
+};
